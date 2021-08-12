e697e96619a17f0098363694fd2a519f06cca42b
Gurpreet Multani <GURPREETSINGHMULTANI@icloud.com>
2021-08-11 22:56:42 -0400

    Updates

---
+++ precache-manifest.08643ce59a2fa50af163c277a2a086ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d41f97df1b1b0172799a",
+    "url": "/Lights-Out-React-App/static/css/main.6d848c62.chunk.css"
+  },
+  {
+    "revision": "d41f97df1b1b0172799a",
+    "url": "/Lights-Out-React-App/static/js/main.d41f97df.chunk.js"
+  },
+  {
+    "revision": "a989bfac1d920b4f2190",
+    "url": "/Lights-Out-React-App/static/js/1.a989bfac.chunk.js"
+  },
+  {
+    "revision": "683dc9d5eae1afdd791c",
+    "url": "/Lights-Out-React-App/static/js/runtime~main.683dc9d5.js"
+  },
+  {
+    "revision": "c17907e06c01e9adae7d5b3d08475757",
+    "url": "/Lights-Out-React-App/index.html"
+  }
+];
